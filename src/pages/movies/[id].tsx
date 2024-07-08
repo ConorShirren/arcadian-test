bf2af37bcfa9b3a625a08f8d873e8195a9045791
Conor Shirren <cshirren@gmail.com>
2024-07-08 20:53:26 +1000

    fix: styling updates

--- src/pages/movies/[id].tsx
+++ src/pages/movies/[id].tsx
@@ -1,9 +1,13 @@
+// pages/movies/[id].tsx
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/yeti/bootstrap.min.css';
+
+import { Card, Col, Row } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 
-import { Card } from 'react-bootstrap';
-import HeaderComponent from '@/app/components/Header';
+import HeaderComponent from '../../app/components/Header';
 import Image from 'next/image';
-// pages/movies/[id].tsx
 import { useRouter } from 'next/router';
 
 interface Movie {
@@ -42,25 +46,30 @@ const MovieDetails = () => {
     <div>
       <HeaderComponent/>
       <div className="container">
-        <Card className='my-3 p-3 rounded'>
-        {movie.Poster !== 'N/A' && <Card.Img variant="top" src={movie.Poster} />}
-          <Card.Body>
-            <Card.Title as='div' className='product-title'>
-            <strong>{movie.Title}</strong>
-            </Card.Title>
-            <br/>
-            <Card.Text as='div'>{movie.Plot}</Card.Text>
-            <br/>
-            <Card.Text as='div'><b>Released:</b> {movie.Year}</Card.Text>
-            <Card.Text as='div'><b>Genre:</b> {movie.Genre}</Card.Text>
-            <Card.Text as='div'><b>Runtime:</b> {movie.Runtime}</Card.Text>
-            <Card.Text as='div'><b>Main Cast:</b> {movie.Actors}</Card.Text>
-            <Card.Text as='div'><b>Languages:</b> {movie.Language}</Card.Text>
-            <Card.Text as='div'><b>Awards:</b> {movie.Awards}</Card.Text>
-            <Card.Text as='div'><b>Box Office:</b> {movie.BoxOffice}</Card.Text>
-            <Card.Text as='div'><b>IMDB Score:</b> {movie.imdbRating}</Card.Text>
-          </Card.Body>
-        </Card>
+        <Row lg={2} >
+          <Card className='my-3 p-3 rounded'>
+            <Card.Body>
+              <Card.Title as='div' className='product-title'>
+              <strong>{movie.Title}</strong>
+              </Card.Title>
+              <br/>
+              <Card.Text as='div'>{movie.Plot}</Card.Text>
+              <br/>
+              <Card.Text as='div'><b>Released:</b> {movie.Year}</Card.Text>
+              <Card.Text as='div'><b>Genre:</b> {movie.Genre}</Card.Text>
+              <Card.Text as='div'><b>Runtime:</b> {movie.Runtime}</Card.Text>
+              <Card.Text as='div'><b>Main Cast:</b> {movie.Actors}</Card.Text>
+              <Card.Text as='div'><b>Languages:</b> {movie.Language}</Card.Text>
+              <Card.Text as='div'><b>Awards:</b> {movie.Awards}</Card.Text>
+              <Card.Text as='div'><b>Box Office:</b> {movie.BoxOffice}</Card.Text>
+              <Card.Text as='div'><b>IMDB Score:</b> {movie.imdbRating}</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className='my-3 p-3 rounded'>
+            {movie.Poster !== 'N/A' && <Card.Img variant="bottom" src={movie.Poster} />}
+          </Card>
+          {/* </Col> */}
+        </Row>
       </div>
     </div>
   );
